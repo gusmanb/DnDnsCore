585744f4ca396f15e950f2206f3c9878b6826dfa
geniwab@gmail.com <geniwab@gmail.com>
2016-09-04 06:56:09 +0200

    Agregar archivos de proyecto.

---
+++ src/DnDnsCore/Security/IMessageSecurityProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DnDnsCore.Query;
+
+namespace DnDnsCore.Security
+{
+    public interface IMessageSecurityProvider
+    {
+        DnsQueryRequest SecureMessage(DnsQueryRequest dnsQueryRequest);
+    }
+}
